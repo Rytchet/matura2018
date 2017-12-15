c45dc2cc2e792f6138b03234963f34b5848d33ff
Rytchet <oskarino.g@gmail.com>
2017-12-15 22:35:11 +0100

    Rozwiązane 59.1

--- 59/59.cpp
+++ 59/59.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <cmath>
 #include <fstream>
-#include <string>
+#include <vector>
 using namespace std;
 
 /* Treœæ
@@ -10,7 +9,7 @@ Input: "wyniki_liczby.txt"
 */
 
 /* Wyniki
-
+59.1: 122
 */
 
 const int SIZE = 1000;
@@ -22,19 +21,40 @@ pierwsza, która dzieli tê liczbê ca³kowicie. Podaj, ile jest w pliku liczby.txt
 liczb, w których rozk³adzie na czynniki pierwsze wystêpuj¹ dok³adnie trzy ró¿ne 
 czynniki (mog¹ siê one powtarzaæ), z których ka¿dy jest nieparzysty.
 */
+
 void z1() {
-  int n;
+  cout << endl << "Zadanie 1: " << endl;
+  int n, primeCount, numberCount = 0;
+  bool flag;
   for (int i = 0; i < SIZE; i++) {
+    primeCount = 0;
     n = tab[i];
+    flag = true;
     while (n != 1) {
-      n = tab[i]
-      
+      if (n % 2 == 0) {
+        flag = false;
+        break;
+      }
+      for (int i = 3; i <= n; i = i + 2) {
+        if (n % i == 0) {
+          primeCount++;
+          while (n % i == 0 ) {
+            n = n / i;
+          }
+          break;
+        }
+      }
+      if (primeCount > 3) break;
     }
+    if (primeCount != 3) flag = false;
+    if (flag) numberCount++;
+  }
+  cout << "Ilosc liczb: " << numberCount << endl;
 }
 
 int main() {
   ifstream in;
-  in.open("wyniki_liczby.txt");
+  in.open("liczby.txt");
   for (int i = 0; i < SIZE; i ++) {
     in >> tab[i];
   }
