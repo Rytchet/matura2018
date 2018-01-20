b532d1c4dd8318f71e3d443370213f37c5bca544
Rytchet <oskarino.g@gmail.com>
2018-01-20 19:43:45 +0100

    Rozwiązane 59

--- 59/59.cpp
+++ 59/59.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 using namespace std;
 
 /* Treœæ
@@ -9,7 +10,8 @@ Input: "wyniki_liczby.txt"
 */
 
 /* Wyniki
-59.1: 122
+59.1: 114
+
 */
 
 const int SIZE = 1000;
@@ -49,7 +51,91 @@ void z1() {
     if (primeCount != 3) flag = false;
     if (flag) numberCount++;
   }
-  cout << "Ilosc liczb: " << numberCount << endl;
+  cout << numberCount << endl;
+}
+
+/*
+Podaj, ile jest w pliku liczby.txt liczb, dla których suma danej liczby i 
+liczby odwróconej jest liczb¹ palindromiczn¹, tzn. jej zapis dziesiêtny 
+jest palindromem. 
+*/
+
+int reverse(int n) {
+  int result = 0;
+  while (n > 0) {
+    result = 10 * result + n % 10;
+    n = n / 10;
+  }
+  return result;
+}
+
+void z2() {
+  cout << "Zadanie 2: " << endl;
+  int counter = 0;
+  int result;
+  for (int i = 0; i < SIZE; i++) {
+    result = tab[i] + reverse(tab[i]);
+    if (reverse(result) == result) counter++;
+  }
+  cout << counter << endl;
+}
+
+/*
+Niech w(n) oznacza iloczyn cyfr liczby n. Dla danej liczby n tworzymy ci¹g, 
+w którym kolejny element jest iloczynem cyfr wystêpuj¹cych w poprzednim 
+elemencie. Ci¹g koñczy siê, gdy liczba jest liczb¹ jednocyfrow¹. Wówczas moc¹ 
+liczby n jest liczba k. 
+Podaj, ile jest w pliku liczby.txt liczb o mocy 1, 2, 3, ..., 8. 
+Dodatkowo podaj minimaln¹ i maksymaln¹ liczbê o mocy równej 1. 
+
+Przyk³ad:
+Liczba 678 ma moc 4, poniewa¿:
+6 * 7 * 8 = 336
+3 * 3 * 6 = 54
+5 * 4 = 20
+2 * 0 = 0
+Liczba 1991 ma moc 2, poniewa¿
+1 * 9 * 9 * 1 = 81
+8 * 1 = 8 
+*/
+
+int w(int n) {
+  int product = 1;
+  int number;
+  while (n > 0) {
+    number = n % 10;
+    n = n / 10;
+    product = product * number;
+  }
+  return product;
+}
+
+void z3() {
+  cout << "Zadanie 3: " << endl;
+  int k, product;
+  int result[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+  int min = 999999, max = 0;
+  for (int i = 0; i < SIZE; i++) {
+    k = 0;
+    product = tab[i];
+    do {
+      product = w(product);
+      k++;
+    } while (product > 9);
+    if (k < 9) {
+      result[k] = result[k] + 1;
+    }
+    if (k == 1) {
+      if (tab[i] > max) max = tab[i];
+      if (tab[i] < min) min = tab[i];
+    }
+  }
+  cout << "Minimum k = 1: " << min << endl;
+  cout << "Maksimum k = 1: " << max << endl;
+  for (int i = 1; i < 9; i++) {
+    cout << i << ": " << result[i] << "; ";
+  }
+  cout << endl;
 }
 
 int main() {
@@ -61,4 +147,6 @@ int main() {
   in.close();
   
   z1();
+  z2();
+  z3();
 }
